c9eb2af295fd701d6ddbbc654e2974bb8921edd7
Emily Harber <emily.a.harber@gmail.com>
2019-08-17 15:12:26 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+  new = []
+  array.length.times do |x|
+    new << yield(array[x])
+  end
+  new
+end 
+
+def reduce(array, starting_value = nil)
+  
+  if starting_value
+    value = starting_value
+    i = 0
+  else
+    value = array[0]
+    i = 1
+  end
+  
+  while i < array.length 
+    value = yield(value, array[i])
+    i += 1 
+  end
+  
+  value 
+  
+end 
