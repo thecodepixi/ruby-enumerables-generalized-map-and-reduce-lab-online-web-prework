00b255e5b0a287c9fd53e58f206303cda19f317f
Emily Harber <emily.a.harber@gmail.com>
2019-08-17 14:28:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(array)
+  new = []
+  array.length.times do |x|
+    new.push(yield(array[x]))
+  end
+  new
+end 
+
+def reduce(array, starting_value = 0)
+  new =[]
+  value = starting_value 
+  
+  array.length.times do |x|
+    value += array[x]
+  end
+  
+  new.push(value)
+  
+  new[0] 
+  
+end 
